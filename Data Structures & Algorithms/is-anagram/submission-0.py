65de41db689065329212aaeb0320e568d1818aaf
ezan <enricozanetti1998@gmail.com>
2026-09-18 17:22:13 +0200

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_letters = {}
+        t_letters = {}
+
+        for l in s:
+            if l in s_letters:
+                s_letters[l] += 1
+            else:
+                s_letters[l] = 1
+            
+        for l in t:
+            if l in t_letters:
+                t_letters[l] += 1
+            else:
+                t_letters[l] = 1
+
+        if s_letters == t_letters:
+            return True
+        else:
+            return False
+        
